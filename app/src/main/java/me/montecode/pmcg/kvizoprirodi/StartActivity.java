75a7933b4cd0f8fff8f0036b009b6eba1badee0d
Stevan Bogosavljević <stevan_nk@ymail.com>
2014-11-12 22:00:48 +0100

    Disabled menu in StartActivity, added new launcher icon

--- app/src/main/java/me/montecode/pmcg/kvizoprirodi/StartActivity.java
+++ app/src/main/java/me/montecode/pmcg/kvizoprirodi/StartActivity.java
@@ -1,9 +1,7 @@
 package me.montecode.pmcg.kvizoprirodi;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class StartActivity extends ActionBarActivity {
@@ -14,26 +12,32 @@ public class StartActivity extends ActionBarActivity {
         setContentView(R.layout.start_activity_layout);
     }
 
+//TODO EVERYTHING
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start_activity_menu, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    //MENU CODE (not needed atm)
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.start_activity_menu, menu);
+//        return true;
+//    }
 
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+//        return super.onOptionsItemSelected(item);
+//    }
+
+
 }
